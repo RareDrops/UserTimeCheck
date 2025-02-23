854cca677f098e184ff8c846189e77c71a0e2fef
RareDrops <54132759+RareDrops@users.noreply.github.com>
2025-02-23 21:16:22 +1300

    v2

--- src/app.js
+++ src/app.js
@@ -3,16 +3,27 @@ const utc = require('dayjs/plugin/utc');          // Import UTC plugin
 const timezone = require('dayjs/plugin/timezone');  // Import timezone plugin
 dayjs.extend(utc);        // Extend with UTC first
 dayjs.extend(timezone);   // Then extend with timezone
+import MicroModal from 'micromodal';  // es6 module
+MicroModal.init();
 
-const userTimezone = "Pacific/Auckland";
+// Automatically detect the visitor's timezone
+var userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 // Define the updateTime function
 function updateTime() {
     const currentDate = dayjs().tz(userTimezone);
-    document.getElementById("user-timezone").textContent = userTimezone;
+    const displayedTimezone = userTimezone.replace(/_/g, " ");
+    document.getElementById("user-timezone").textContent = displayedTimezone;
     document.getElementById("current-time").textContent = currentDate.format("HH:mm:ss");
     document.getElementById("current-date").textContent = currentDate.format("dddd, D MMMM, YYYY");
   }
+
+// Handle timezone selection
+document.getElementById("timezone-select").addEventListener("change", function() {userTimezone = this.value;});
+
+// Handle the Apply button click
+document.getElementById("timezone-select-btn").addEventListener("click", function() {updateTime();});
+
 
 
 setInterval(updateTime, 1000);  // Set the interval to 1000ms (1 second)
