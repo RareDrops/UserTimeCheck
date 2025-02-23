0c607b8eeaa682dc7797e5767a781b2a86f0bdc5
RareDrops <54132759+RareDrops@users.noreply.github.com>
2025-02-24 01:37:20 +1300

    added sorting functionality
    sorts the json first before adding the options to make the options alphabetical

--- public/script.js
+++ public/script.js
@@ -2,6 +2,20 @@
 fetch('timezones.json')
 	.then(response => response.json())
 	.then(data => {
+
+		// Sort the timezones alphabetically by label
+		data.sort((a, b) => {
+			const labelA = a.label.toUpperCase(); // Ignore upper and lowercase
+			const labelB = b.label.toUpperCase(); // Ignore upper and lowercase
+			if (labelA < labelB) {
+				return -1;
+			}
+			if (labelA > labelB) {
+				return 1;
+			}
+			return 0;
+		});
+		
 		const select = document.getElementById("timezone-select");
 		data.forEach(timezone => {
 			const option = document.createElement("option");
